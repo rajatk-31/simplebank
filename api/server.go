d313eba45a499008519fcef74da6022e31ece962
Rajat <rajatky3107@gmail.com>
2023-01-09 14:49:11 +0530

    custom params  validator

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/rajatk-31/simplebank/db/sqlc"
 )
 
@@ -14,7 +16,9 @@ func NewServer(store db.Store) *Server {
 
 	server := &Server{store: store}
 	router := gin.Default()
-
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
