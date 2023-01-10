6cd4f9e7532684d533f6e87e60a8192a8b7e266c
Rajat <rajatky3107@gmail.com>
2023-01-10 16:21:21 +0530

    Create user api

--- api/server.go
+++ api/server.go
@@ -19,6 +19,8 @@ func NewServer(store db.Store) *Server {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+
+	router.POST("/user", server.createUser)
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
---
+++ api/user.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	db "github.com/rajatk-31/simplebank/db/sqlc"
+	"github.com/rajatk-31/simplebank/util"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	Email    string `json:"email" binding:"required,email"`
+	FullName string `json:"fullname" binding:"required"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	hashed, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		FullName:       req.FullName,
+		Email:          req.Email,
+		HashedPassword: hashed,
+	}
+
+	account, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqError, ok := err.(*pq.Error); ok {
+			switch pqError.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+			log.Println(pqError.Code.Name())
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
